c2e860806e94db352c27cef35e02d00b960ae1f7
Markus Bergum Hilbertsen <markus@orcalabs.no>
2025-02-17 08:38:08 +0100

    remove routing & unnecessary dependencies

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,9 @@
 import { Component } from "@angular/core";
-import { RouterOutlet } from "@angular/router";
+import { HomePageComponent } from "./pages/home-page/home-page.component";
 
 @Component({
   selector: "app-root",
-  imports: [RouterOutlet],
+  imports: [HomePageComponent],
   templateUrl: "./app.component.html",
   styleUrl: "./app.component.scss",
 })

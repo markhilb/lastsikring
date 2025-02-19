4304e3221dc0cf95d8cc2bd17e22f40acef75a68
Markus Bergum Hilbertsen <markus@orcalabs.no>
2025-02-19 12:46:23 +0100

    remove unused import

--- src/app/components/overfallsikring-straps/overfallsikring-straps.component.ts
+++ src/app/components/overfallsikring-straps/overfallsikring-straps.component.ts
@@ -1,11 +1,10 @@
 import { Component } from "@angular/core";
 import { VariablesService } from "../../services/variables.service";
-import { NumberInputComponent } from "../number-input/number-input.component";
 import { OverfallsikringStrapsEquationComponent } from "../overfallsikring-straps-equation/overfallsikring-straps-equation.component";
 
 @Component({
   selector: "app-overfallsikring-straps",
-  imports: [NumberInputComponent, OverfallsikringStrapsEquationComponent],
+  imports: [OverfallsikringStrapsEquationComponent],
   templateUrl: "./overfallsikring-straps.component.html",
   styleUrl: "./overfallsikring-straps.component.scss",
 })
